7226a861d2e8df177c01bc19f39c9cc2b100bb3c
Carlos <uppaplicativo@gmail.com>
2024-08-27 13:23:39 -0300

    first commit

--- src/features/qrCodeConfirm.tsx
+++ src/features/qrCodeConfirm.tsx
@@ -13,9 +13,9 @@ export default function QrCodeConfirm() {
       <div className="w-full justify-center items-center flex flex-col gap-2 max-w-xl mx-auto px-4">
         <Alert className="my-2 bg-blue-100">
           <BsExclamationOctagonFill size={25} className="fill-blue-500" />
-          <AlertTitle className="text-blue-800 ml-1">Heads up!</AlertTitle>
+          <AlertTitle className="text-blue-800 ml-1">Mostre este código QR a um funcionário!</AlertTitle>
           <AlertDescription className="ml-1 text-blue-800">
-            You can add components to your app using the cli.
+          Você tem 69.820 pontos. Após recuperar esta recompensa, você terá 67.320 pontos restantes.
           </AlertDescription>
         </Alert>
         <QRCodeCanvas size={350} value="carlos roberto rosa da Silva" />
