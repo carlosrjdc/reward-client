aa6802ec40be612c06dd931048f8003d80eb2df0
Carlos <uppaplicativo@gmail.com>
2024-08-27 10:39:24 -0300

    first commit

--- src/features/homePage/components/steps.tsx
+++ src/features/homePage/components/steps.tsx
@@ -16,7 +16,10 @@ const StepperIndicator = ({ activeStep }: StepperIndicatorProps) => {
             </span>
             <div className="flex justify-between">
               <div>
-                <h3 className="font-medium leading-tight">Compra</h3>
+                <div className="flex gap-2">
+                  <h3 className="font-medium leading-tight">Compra</h3>
+                  <span className="text-[10px] font-bold p-0.5 rounded bg-yellow-500/50">R$20,00</span>
+                </div>
                 <p className="text-sm">+500 pontos</p>
               </div>
               <div className="flex flex-col items-end">
