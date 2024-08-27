6d21683d1b3a34bc65c3c5e176d84f348f977f96
Carlos <uppaplicativo@gmail.com>
2024-08-27 09:51:15 -0300

    first commit

--- src/features/homePage/components/cardBalance.tsx
+++ src/features/homePage/components/cardBalance.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Dispatch, useState } from "react";
 import { IoQrCodeSharp } from "react-icons/io5";
 import { FaCoins } from "react-icons/fa";
+import { QRCodeCanvas } from "qrcode.react";
 
 export default function CardBalance() {
   const [stateQrCode, setStateQrCode] = useState(true);
@@ -35,11 +36,16 @@ export default function CardBalance() {
             </div>
           </CardContent>
         ) : (
-          <CardContent className="flex mt-4">
-            <CardQrdCode handle={setStateQrCode} />
+          <CardContent className="flex mt-4 items-center gap-4 h-full">
+            <QRCodeCanvas
+            
+              size={140}
+              onClick={() => setStateQrCode(true)}
+              value="654521-655521-1998721"
+            />
             <div className="flex flex-col justify-between items-start p-2">
               <div className="flex flex-col">
-                <strong className="font-bold text-3xl">Carlos Rosa</strong>
+                <strong className="font-bold text-xl">Carlos Rosa</strong>
                 <div className="flex gap-2 items-center">
                   <FaCoins />
                   <span className="font-semibold text-lg">1.980</span>
@@ -60,11 +66,7 @@ export default function CardBalance() {
 export const CardQrdCode = ({ handle }: { handle: Dispatch<boolean> }) => {
   return (
     <div onClick={() => handle(true)}>
-      <img
-        className="w-48 h-full bg-red-500"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuIy6HNc3zXzJ9-y-rNEfnaSdhcgeXytmnQg&s"
-        alt=""
-      />
+      <QRCodeCanvas value="654521-655521-1998721" />
     </div>
   );
 };
