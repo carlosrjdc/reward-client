cdadaf3d796e9d7d60698af8cc0223baaa0dcb4f
Carlos <uppaplicativo@gmail.com>
2024-08-27 11:48:12 -0300

    first commit

--- src/features/qrCodeConfirm.tsx
+++ src/features/qrCodeConfirm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { HeaderMenu } from "@/components/personizado/header";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Card, CardContent } from "@/components/ui/card";
+import { BsExclamationOctagonFill } from "react-icons/bs";
 import { RocketIcon } from "@radix-ui/react-icons";
 import { QRCodeCanvas } from "qrcode.react";
 import Reward from "./homePage/components/reward";
@@ -10,11 +10,11 @@ export default function QrCodeConfirm() {
   return (
     <div>
       <HeaderMenu />
-      <div className="w-full justify-center items-center flex flex-col gap-2 max-w-xl mx-auto px-4">
-        <Alert>
-          <RocketIcon className="h-4 w-4" />
-          <AlertTitle>Heads up!</AlertTitle>
-          <AlertDescription>
+      <div className="w-full text-slate-900 justify-center items-center flex flex-col gap-2 max-w-xl mx-auto px-4">
+        <Alert className="my-2 bg-blue-100">
+          <BsExclamationOctagonFill size={25} className="fill-blue-500" />
+          <AlertTitle className="text-blue-800 ml-1">Heads up!</AlertTitle>
+          <AlertDescription className="ml-1 text-blue-800">
             You can add components to your app using the cli.
           </AlertDescription>
         </Alert>
