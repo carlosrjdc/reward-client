b81cd2e25cfddae9c1b3cdeee2b326554a9d194d
Carlos <uppaplicativo@gmail.com>
2024-08-27 11:37:02 -0300

    first commit

--- src/components/personizado/header.tsx
+++ src/components/personizado/header.tsx
@@ -4,13 +4,15 @@ import { MdDarkMode } from "react-icons/md";
 import { FaCoins } from "react-icons/fa6";
 import { CiLight } from "react-icons/ci";
 import { useTheme } from "next-themes";
+import { useRouter } from "next/navigation";
 
 export function HeaderMenu() {
   const { setTheme, theme } = useTheme();
+  const router = useRouter()
   return (
     <header className="flex justify-between sticky p-2 top-0 z-50 w-full bg-secondary/95 backdrop-blur supports-[backdrop-filter]:bg-secondary/60">
       <div className="flex items-center gap-4">
-        <FaCoins size={40} />
+        <FaCoins onClick={()=> router.push("/")} size={40} />
         <strong className="text-primary">SISTEMA FIDELIDADE</strong>
       </div>
       <div className="flex justify-between gap-4 items-center">
