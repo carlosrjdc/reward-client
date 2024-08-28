39550a9bb7c894d21bded5a1f551336b9c1ddffc
Carlos <uppaplicativo@gmail.com>
2024-08-28 19:06:04 -0300

    first commit

--- src/features/redeem/index.tsx
+++ src/features/redeem/index.tsx
@@ -8,9 +8,21 @@ export default function Redeem() {
       <HeaderMenu />
       <div className="w-full flex flex-col gap-2 max-w-xl mx-auto px-4">
         <CardRedeem />
-        <Reward />
-        <Reward />
-        <Reward />
+        <Reward
+          content="Corte de cabelo barbearia do zé"
+          name="Corte Cabelo"
+          points={120}
+        />
+        <Reward
+          content="Servico de BarbaTerapia"
+          name="BarbaTerapia"
+          points={230}
+        />
+        <Reward
+          content="servico de sombracelha masculina"
+          name="Sombracelha masculina"
+          points={80}
+        />
       </div>
     </div>
   );
